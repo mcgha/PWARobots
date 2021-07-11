8a4819e4d48fc6dfadd93b6eec14d2dbce497308
Marcos Campos <marcoscampos@Marcoss-MacBook-Pro.local>
2021-07-11 12:06:36 -0400

    added snapshot testing and unit testing for Card, CardList, CounterButton components

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -5,7 +5,7 @@ const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
       <input
-        className='pa3 ba b--green bg-lightest-blue'
+        className='pa3 ba b--blue bg-lightest-blue'
         type='search'
         placeholder='search robots'
         onChange={searchChange}
